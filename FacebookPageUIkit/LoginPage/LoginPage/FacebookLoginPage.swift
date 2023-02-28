01d6120a193c1a66ffa4f1ce8fee9c6dc1dca879
GabrielSOliv <100054103+GabrielSOliv@users.noreply.github.com>
2023-02-28 20:45:06 -0300

    Organizando Arquivos

---
+++ FacebookPageUIkit/LoginPage/LoginPage/FacebookLoginPage.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Gabriel Oliveira on 28/02/23.
+//
+
+import UIKit
+
+class FacebookLoginPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor =
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
